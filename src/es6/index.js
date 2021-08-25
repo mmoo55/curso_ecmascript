2f3fa18b73784129e61cdbbf77bdc78f8d887f35
mmoo55 <g.gerson51@gmail.com>
2021-08-25 19:02:06 -0400

    Comentando el código

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,4 @@
+// Como era antes de ES6
 function newFunction(name, age, country) {
     var name = name || 'Oscar';
     var age = age || '32';
@@ -5,19 +6,21 @@ function newFunction(name, age, country) {
     console.log(name, age, country);
 }
 
-// es6
+// con ES6
 function newFunction2(name = 'Oscar', age = 32, country = "BO") {
     console.log(name, age, country);
 };
 
+// Llamando a las funciones de dos maneras
 newFunction2(); // Toma los elementos por defecto
 newFunction2('Ricardo', '23', 'CO');
 
-
+// Concatenación antes de ES6
 let hello = "Hello";
 let world = "World";
 let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
-// Template literals
+
+// Con ES6 se implementó Template literals, usando ${} trae el valor de la variable
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
