b96c2f4c8d62449077c023ef1c85397a928d546f
mmoo55 <g.gerson51@gmail.com>
2021-08-26 14:41:36 -0400

    Let, Const, Multilinea, operator y destructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -24,3 +24,58 @@ console.log(epicPhrase);
 // Con ES6 se implementó Template literals, usando ${} trae el valor de la variable
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+// Multilinea
+// Antes
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
++ "otra frase épica que necesitamos."
+
+// ES6
+let lorem2 = `otra frase épica que necesitamos
+ahora es otra frase épica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuración de elementos
+// Antes
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+// ES6
+let { name } = person;
+console.log(name);
+
+// Operador de propagación (nos permite expandir de ciertas situaciones varios elementos)
+// ES6
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2]; // union de los dos array
+
+console.log(education);
+
+
+// Scope con let y var
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+// const
+const a = 'b';
+a = 'a';
+console.log(a);
