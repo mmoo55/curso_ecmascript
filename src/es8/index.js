27c390ed60d57e70f7d84590bca8e0c87bfc0319
mmoo55 <g.gerson51@gmail.com>
2021-08-29 17:15:31 -0400

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -33,3 +33,31 @@ console.log('food'.padEnd(12, '  -----'))
 const object = {
     name: 'oscar',
 }
+
+// Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    // const hello2 = await --- otra función
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
