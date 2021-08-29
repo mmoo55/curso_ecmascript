0b785b4568897a0dd68bedf872653e445456b4dd
mmoo55 <g.gerson51@gmail.com>
2021-08-29 15:32:23 -0400

    ES7, includes, exponentes

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// Includes; permite saber si hay un elemento dentro de un string o array
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra');
+}
+
+// Exponentes o elevar a la potencia
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
